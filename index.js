c09c9f43a9d2242797ac56e86d37de41a1de094d
João Carvalho <joao.mendes.carvalh@gmail.com>
2019-09-08 20:52:10 +0100

    Major clean up
    Removed a project.
    Removed sliders.
    Removed dead CSS entries.
    Removed text from links.
    Changed the arrow.

--- index.js
+++ index.js
@@ -3,29 +3,4 @@ $(document).ready(() => {
         $(event.currentTarget).parent().siblings('.flavour').toggle();
         $(event.currentTarget).toggleClass('flipped');
     })
-
-    $('.arrow').mouseenter(function () {
-        $(this).attr('src', 'images/graphics/arrow.gif');
-    })
-    $('.arrow').mouseleave(function () {
-        $(this).attr('src', 'images/graphics/arrow.png');
-    })
 })
-
-var slideIndex = 1;
-showSlide(slideIndex);
-
-function plusSlide(n) {
-    showSlide(slideIndex += n);
-}
-
-function showSlide(n) {
-    var i;
-    var x = document.getElementsByClassName("slide");
-    if (n > x.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = x.length };
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[slideIndex - 1].style.display = "block";
-}
